458b920aec419f2029add7d0742c3964420e5eb1
kavirajkv <kavirajk36kv@gmail.com>
2025-03-09 12:17:59 +0530

    api to get transactions added

--- institutions-demo/banks/main.go
+++ institutions-demo/banks/main.go
@@ -12,10 +12,12 @@ import (
 	"time"
 )
 
-type accoutreq struct {
+// account discovery usiing phone type
+type accountreq struct {
 	Phone string `json:"phone"`
 }
 
+// if accound found return type for response
 type accountres struct {
 	Accno  string `json:"acc_no"`
 	Ifsc   string `json:"ifsc"`
@@ -23,24 +25,27 @@ type accountres struct {
 	Branch string `json:"branch"`
 }
 
+// transactions request type
 type transactionreq struct {
 	Accno string `json:"acc_no"`
+	Bank  string `json:"bank"`
 }
 
+// return type for transactions data request
 type transactionres struct {
-	Accno        string      `json:"acc_no"`
-	Ifsc         string      `json:"ifsc"`
-	Transactions transaction `json:"transactions"`
+	Accno        string        `json:"acc_no"`
+	Transactions []transaction `json:"transactions"`
 }
 
 type transaction struct {
 	Transactionid int       `json:"transaction_id"`
 	Type          string    `json:"transaction_type"`
 	Mode          string    `json:"transaction_mode"`
-	Amount        int       `json:"amount"`
+	Amount        float64   `json:"amount"`
 	Time          time.Time `json:"time"`
 }
 
+// db connection based on database name
 func ConnectDB(dbname string) *sql.DB {
 	db_pass := os.Getenv("PG_PASSWORD")
 	dsn := fmt.Sprintf("postgres://postgres:%v@localhost/%v?sslmode=disable", db_pass, dbname)
@@ -54,38 +59,75 @@ func ConnectDB(dbname string) *sql.DB {
 
 }
 
+// routes
 func route() *mux.Router {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/icici/get_account", get_account("icici")).Methods("POST", "OPTIONS")
 	router.HandleFunc("/axis/get_account", get_account("axis")).Methods("POST", "OPTIONS")
 	router.HandleFunc("/hdfc/get_account", get_account("hdfc")).Methods("POST", "OPTIONS")
+	router.HandleFunc("/get_transactions", get_transactions).Methods("POST", "OPTIONS")
 
 	return router
 }
 
-func get_account(db string)http.HandlerFunc{
+// this function will search for account based on user phone number if found return details
+func get_account(db string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-	var accreq accoutreq
+		var accreq accountreq
 
-	err := json.NewDecoder(r.Body).Decode(&accreq)
-	if err != nil {
-		http.Error(w, "Enter string message", http.StatusBadRequest)
+		err := json.NewDecoder(r.Body).Decode(&accreq)
+		if err != nil {
+			http.Error(w, "Enter valid phone number", http.StatusBadRequest)
+		}
+
+		db := ConnectDB(db)
+		defer db.Close()
+		statement := "SELECT account_number,ifsc_code,account_type,branch_name FROM accounts WHERE user_id = (SELECT user_id FROM users WHERE phone_number = $1);"
+		row := db.QueryRow(statement, accreq.Phone)
+		var accres accountres
+		errors := row.Scan(&accres.Accno, &accres.Ifsc, &accres.Type, &accres.Branch)
+		if errors != nil {
+			http.Error(w, "account not found", http.StatusNotFound)
+
+		}
+
+		json.NewEncoder(w).Encode(accres)
+
 	}
-
-	db := ConnectDB(db)
-	defer db.Close()
-	statement := "SELECT account_number,ifsc_code,account_type,branch_name FROM accounts WHERE user_id = (SELECT user_id FROM users WHERE phone_number = $1);"
-	row := db.QueryRow(statement, accreq.Phone)
-	var accres accountres
-	errors := row.Scan(&accres.Accno, &accres.Ifsc, &accres.Type, &accres.Branch)
-	if errors != nil {
-		log.Fatalf("error while converting data -%v", errors)
-	}
-
-	json.NewEncoder(w).Encode(accres)
-
 }
+
+// this function will retern all the transaction based on acc number
+func get_transactions(w http.ResponseWriter, r *http.Request) {
+	var transactionrequest transactionreq
+
+	err := json.NewDecoder(r.Body).Decode(&transactionrequest)
+	if err != nil {
+		http.Error(w, "Enter valid account number", http.StatusBadRequest)
+	}
+
+	db := ConnectDB(transactionrequest.Bank)
+	defer db.Close()
+	statement := "select transaction_id,transaction_type,mode,amount,transaction_timestamp from transactions where account_id=(select account_id from accounts where account_number=$1);"
+	rows, _ := db.Query(statement, transactionrequest.Accno)
+	var response transactionres
+
+	var transactions []transaction
+	for rows.Next() {
+		var trans transaction
+		errors := rows.Scan(&trans.Transactionid, &trans.Type, &trans.Mode, &trans.Amount, &trans.Time)
+		if errors != nil {
+			http.Error(w, "account not found", http.StatusNotFound)
+
+		}
+		transactions = append(transactions, trans)
+
+	}
+	response.Accno = transactionrequest.Accno
+	response.Transactions = transactions
+
+	json.NewEncoder(w).Encode(response)
+
 }
 
 func main() {
